455d1ef9557591ac34196493e214a83b5c9649e7
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-08-27 16:51:19 +0700

    add max file size 16 MB

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ app.config['UPLOAD_FOLDER'] = './static/uploads'
 app.config['STATUS'] = ''
 app.config['HOT_AREA'] = ''
 app.config['PREDIKSI_TANGGAL'] = ''
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
 mysql = MySQL(app)
 
